29e15c56b8ef84a66764744ef0fdffb298134b15
SharpMars <70289680+SharpMars@users.noreply.github.com>
2025-01-05 22:49:06 +0100

    Force recreate app container

--- hooks/update.sh
+++ hooks/update.sh
@@ -1,4 +1,4 @@
 cd code
 git config --global --add safe.directory /usr/src/app/code
 git pull
-docker compose up -d --no-deps --build app
+docker compose up -d --no-deps --force-recreate --build app
